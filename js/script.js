91b612538a55bc0df205a9e1cd3c617ca16fc787
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-08-31 23:12:42 +0600

    added button using api

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const loadVideo = async () =>{
+const loadVideo = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const getData = await response.json();
     const data = getData.data;
@@ -8,15 +8,15 @@ const loadVideo = async () =>{
 const displayData = (data) => {
 
     console.log(data);
-    
+
+    const tapContainer = document.getElementById('tap-container');
+    tapContainer.classList = `flex justify-center gap-4`;
 
     data.forEach(category => {
-        const tapContainer = document.getElementById('tap-container');
-        const div = document.createElement('div')
-        div.classList = `flex justify-center gap-4`;
-        div.innerHTML = `<a><button
-        class="btn text-[#171717B2] bg-[#25252526] text-[#252525] rounded-md hover:bg-[#FF1F3D] hover:text-white">${category.category}</button></a>`
-        tapContainer.appendChild(div)
+        const tapDiv = document.createElement('a');
+        tapDiv.innerHTML = `<button
+        class="btn text-[#171717B2] bg-[#25252526] text-[#252525] rounded-md hover:bg-[#FF1F3D] hover:text-white">${category.category}</button>`
+        tapContainer.appendChild(tapDiv)
     });
 
 }

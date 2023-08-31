91e43af28a357d75a5f0eddbc792217b8a811bb1
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-08-31 21:32:45 +0600

    added button

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const loadVideo = async () =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+    console.log(data.data);
+};
+
+loadVideo();
